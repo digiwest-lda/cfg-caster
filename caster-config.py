f143416f3da52a891265659b1d4fbbd947848f13
José Filipe <44379326+JNSFilipe@users.noreply.github.com>
2026-03-31 17:48:34 +0100

    Update image path in caster-config.py
    Signed-off-by: José Filipe <44379326+JNSFilipe@users.noreply.github.com>

--- caster-config.py
+++ caster-config.py
@@ -1,4 +1,4 @@
 CONFIG = {
     "enabled": True,
-    "image_path": "/home/shared/deploy/images/final_images/dtub_golden.img.xz",
+    "image_path": "/home/shared/deploy/images/final_images/lamego_golden.img.xz",
 }
