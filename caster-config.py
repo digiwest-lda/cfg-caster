5cf30848e6a8c2c5c3fddd408b14260a3c1a5c28
José Filipe <44379326+JNSFilipe@users.noreply.github.com>
2026-04-01 14:43:50 +0100

    Update image path in caster-config.py
    Signed-off-by: José Filipe <44379326+JNSFilipe@users.noreply.github.com>

--- caster-config.py
+++ caster-config.py
@@ -1,4 +1,4 @@
 CONFIG = {
     "enabled": True,
-    "image_path": "/home/shared/deploy/images/final_images/lamego_golden.img.xz",
+    "image_path": "/home/shared/deploy/images/final_images/new_golden_aisense_simifta.img.xz",
 }
